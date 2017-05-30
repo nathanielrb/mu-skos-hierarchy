4cbeb238609ac85c21ec863632e55a9bcd9c354e
Nathaniel Rudavsky-Brody <nathaniel.rudavsky@tenforce.com>
2017-05-30 15:29:53 +0200

    generalizing

--- hierarchy.scm
+++ hierarchy.scm
@@ -2,15 +2,18 @@
 ;; - what about those language filters??
 ;; - or get all properties??
 
-(use awful srfi-69)
+(use awful srfi-69 irregex)
 
 (load "sparql.scm")
 (load "rest.scm")
 (load "threads.scm")
 
 (development-mode? #t)
-(debug-file "./debug.log")
+(debug-file "./debug.log") ;; **conf
 (*print-queries?* #t)
+
+(define-namespace skos "http://www.w3.org/2004/02/skos/core#")
+(define-namespace mu "http://mu.semte.ch/vocabularies/core/")
 
 (*default-graph* (or (get-environment-variable "MU_DEFAULT_GRAPH")
                      '<http://data.europa.eu/eurostat/ECOICOP>))
@@ -18,24 +21,44 @@
 (*sparql-endpoint* (or (get-environment-variable "SPARQL_ENDPOINT")
                        "http://172.31.63.185:8890/sparql?"))
 
-(define-namespace taxonomy "http://data.europa.eu/eurostat/id/taxonomy/")
-(define-namespace skos "http://www.w3.org/2004/02/skos/core#")
-(define-namespace mu "http://mu.semte.ch/vocabularies/core/")
-(define-namespace concept "http://data.europa.eu/eurostat/id/taxonomy/ECOICOP/concept/")
-
-(define node-namespace (or "http://data.europa.eu/eurostat/id/taxonomy/ECOICOP/concept/"))
-
-(define-namespace ns node-namespace)
-
 (define scheme (make-parameter
                 (or (get-environment-variable "CONCEPT_SCHEME")
-                    (taxonomy "ECOICOP"))))
+                    '<http://data.europa.eu/eurostat/id/taxonomy/ECOICOP>)))
 
+(define *property-definitions*
+  (or (get-environment-variable "INCLUDED_PROPERTIES")
+      "name=skos:altLabel,description=skos:prefLabel,notation=skos:notation"))
+
+(define *properties*
+  (map (lambda (str) 
+         (map string->symbol (string-split str "="))) 
+       (string-split *property-definitions* ",")))
+
+(define *namespace-definitions*
+  (or (get-environment-variable "NAMESPACES")
+      "skos: http://www.w3.org/2004/02/skos/core#"))
+
+(define *defined-namespaces*
+  (map (lambda (ns) 
+         (print ns)
+         (print (irregex-split ": " ns))
+         (match-let (((prefix uri)
+                      (irregex-split ": " ns)))
+           (register-namespace (string->symbol prefix)                                
+                               uri)))
+       (string-split *namespace-definitions* ",")))
+       
+(define-syntax hit-cache
+  (syntax-rules ()
+    ((hit-property-cache sym prop body)
+     (or (get sym prop)
+         (put! sym prop body)))))
+    
 (define (descendance-query vars scheme child parent)
   (select-triples
    vars
    (s-triples `((,child skos:inScheme ,scheme)
-               (,child skos:broader ,parent)))))
+                (,child skos:broader ,parent)))))
 
 (define (descendants-query node)
   (descendance-query "?x" (scheme) '?x node))
@@ -43,74 +66,61 @@
 (define (ancestors-query node)
   (descendance-query "?x" (scheme) node '?x))
 
-;; this shouldn't be hard-coded
-;; get all properties? or...
-(define (properties-query node)
-  (select-triples
-   "?name, ?description, ?uuid"
-   (conc (s-optional (conc (s-triple `(,node skos:altLabel ?name))
-                           "FILTER (lang(?name) = 'en')\n"))
-         (s-optional (conc (s-triple `(,node skos:prefLabel ?description))
-                           "FILTER (lang(?description) = 'en')\n"))
-         (s-triple `(,node mu:uuid ?uuid)))))
+(define (get-node uuid)
+  (query-unique-with-vars (node)
+     (select-triples
+      "?node"
+      (s-triples `((?node mu:uuid ,uuid))))
+     node))
          
-
-
-(define-syntax hit-cache
-  (syntax-rules ()
-    ((hit-property-cache sym prop body)
-     (or (get sym prop)
-         (put! sym prop body)))))
-
 (define (get-descendants node)
-  (let ((n node)) ;(string->symbol node)))
-    (hit-cache n 'descendants
-               (query-with-vars
-                (x)
-                (descendants-query node) ;(conc "<" node ">")) 
-                x))))
+  (hit-cache node 'descendants
+             (query-with-vars
+              (x)
+              (descendants-query node) ;(conc "<" node ">")) 
+              x)))
 
 (define (get-ancestors node)
-  (let ((n node));  (string->symbol node)))
-    (hit-cache n 'ancestors
-               (query-with-vars 
-                (x)
-                (ancestors-query node);  (conc "<" node ">")) 
-                x))))
+  (hit-cache node 'ancestors
+             (query-with-vars 
+              (x)
+              (ancestors-query node)
+              x)))
 
-(define (car-when l)
-  (if (null? l) '() (car l)))
+(define property-query
+  (match-lambda 
+    ((name predicate)
+     (let ((var (sparql-variable name)))
+       (lambda (node)
+         (cons var
+               (s-optional
+                (s-filter
+                 (s-triple `(,node ,predicate ,var))
+                 (format #f "lang(~A) = 'en' || lang(~A) = ''" var var)))))))))
 
+(define (properties-query node)
+  (let* ((property-queries (map (lambda (pq) (pq node)) 
+                                (map property-query *properties*)))
+         (vars (cons "?uuid" (map ->string (map car property-queries))))
+         (queries (cons (s-triple `(,node mu:uuid ?uuid))
+                        (map cdr property-queries))))
+    (let ((values (car-when
+                   (sparql/select
+                    (select-triples
+                     (string-join vars ", ")
+                     (string-join queries "\n"))))))
+      (filter (lambda (value-pair)
+                (cdr value-pair))
+              values))))
 
 (define (node-properties node)
-  (let ((n node)); (string->symbol node)))
-    (hit-cache n 'properties
-               (append (list (cons 'id (write-uri node))
-                             (cons 'type "TYPE")
-                             (cons 'attributes
-                                   (car-when
-                                    (query-with-vars
-                                     (name description uuid)
-                                     (properties-query node)
-                                     (list
-                                      (cons 'id uuid)
-                                      (cons 'name name)
-                                      (cons 'description description))))))))))
-(define (node-properties node)
-  (let ((n node)); (string->symbol node)))
-    (hit-cache n 'properties
-               (car 
-                (query-with-vars
-                 (name description uuid)
-                 (properties-query node)
-                 `((@id . ,(write-uri node))
-                   (id . ,uuid)
-                   (type . "concept")
-                   (attributes .
-                     (,@(if name `((name . ,name)) '())
-                      ,@(if description `((description . ,description)) '())))))))))
-
-
+  (hit-cache 
+   node 'properties
+   (let ((properties (properties-query node)))
+     `((id . ,(alist-ref 'uuid properties))
+       (type . "concept")
+       (attributes . ,(alist-delete 'uuid properties))))))
+             
 (define (tree next-fn node #!optional levels)
   (if (eq? levels 0)
       (node-properties node)
@@ -130,14 +140,6 @@
 (define (forward-tree node #!optional levels)
   (tree get-descendants node levels))
 
-(define (get-node uuid)
-  (car
-   (query-with-vars (node)
-     (select-triples
-      "?node"
-      (s-triples `((?node mu:uuid ,uuid))))
-      node)))
-
 (define (forward-tree uuid #!optional levels)
   (let ((node (get-node uuid)))
     (tree get-descendants node levels)))
@@ -151,7 +153,7 @@
     (let ((levels ($ 'levels)))
       `((data .
               ,(forward-tree
-                ($path 'id) ;;(ns ($path 'id))
+                ($path 'id)
                 (and levels (string->number levels))))))))
 
 (define-rest-page-fn (($path "/hierarchies/:id/ancestors"))
