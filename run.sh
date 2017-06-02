c4e43057ca778d834f01514bbe9f6b7bbae9a836
Nathaniel Rudavsky-Brody <nathaniel.rudavsky@tenforce.com>
2017-06-02 15:37:12 +0200

    run

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ export SPARQL_ENDPOINT="http://172.31.63.185:8890/sparql"
 
 export CONCEPT_SCHEME="http://data.europa.eu/eurostat/id/taxonomy/ECOICOP"
 
-# export INCLUDED_PROPERTIES="name=skos:altLabel,description=skos:prefLabel,notation=skos:notation"
+export INCLUDED_PROPERTIES="name=skos:altLabel,description=skos:prefLabel,notation=skos:notation"
 
 export DEFAULT_LANGUAGE=en
 
